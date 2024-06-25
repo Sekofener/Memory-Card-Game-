bd34e915c57a77e5b98ec7d2133e24f6c812bac7
Serkan <serkantumur.st@gmail.com>
2024-06-25 05:55:18 -0400

    Meaningful Message Here

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-jpsss  
+/ Helper functions
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
+
+
+
+//Our codes
+var cards =[
+    {value: '2', Image: './images/club_2.svg' }, 
+    {value: '3', image: './images/club_3.svg' },
+    {value: '2', Image: './images/club_2.svg' }, 
+    {value: '3', image: './images/club_3.svg' },
+    {value: '4', Image: './images/club_2.svg' }, 
+    {value: '1', image: './images/club_3.svg' },
+    {value: '1', Image: './images/club_2.svg' }, 
+    {value: '4', image: './images/club_3.svg' },
+]
+var cardEls= document.querySelectorAll('.cards')
+shuffle(card)
